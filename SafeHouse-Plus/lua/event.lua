3298b92529de18290d5a63a3abc36b2218bd6b57
Dr_Newbie <tf2baidonation@gmail.com>
2016-11-04 13:04:00 +0800

    11.3

--- SafeHouse-Plus/lua/event.lua
+++ SafeHouse-Plus/lua/event.lua
@@ -12,7 +12,7 @@ local _MissionScriptElement_on_executed = MissionScriptElement.on_executed
 
 function MissionScriptElement:on_executed(...)
 	local _id = "id_" .. tostring(self._id)
-	if SafeHousePlus and not Network:is_client() then
+	if SafeHousePlus and not Network:is_client() and Global.game_settings and Global.game_settings.level_id == "chill" then
 		--Before Enter
 		if _id == "id_101282" and not SafeHousePlus._tmp[_id] then
 			SafeHousePlus._tmp[_id] = true
